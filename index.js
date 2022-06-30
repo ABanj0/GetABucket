b6e84569116105d91eddac909a8bd86e4c2f93e2
Angelo Ayoub <83978215+ABanj0@users.noreply.github.com>
2022-06-29 23:04:23 -0400

    First Commit

---
+++ index.js
@@ -0,0 +1,12 @@
+// Performing a task
+function greet(name, lastNamme){
+    console.log('Hello ' + name + ' ' + lastNamme);
+}
+
+
+//Calculting a value
+function square(number){
+    return number * number;
+}
+
+console.log(square(2));
